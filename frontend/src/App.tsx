e2b83884d642d702a074906d496f760062115a7e
kacper0276 <kacper0276@op.pl>
2025-03-04 10:55:22 +0100

    [FE] Add basic packages (bootstrap, toastify, i18next)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const header = <h1>Header</h1>;
+
+  const content = (
+    <Routes>
+      <Route path="/" element={<h1>Main Page</h1>} />
+    </Routes>
+  );
+
+  const footer = <h1>Footer</h1>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Layout header={header} content={content} footer={footer} />
+    </Router>
   );
 }
 
--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -5,20 +5,13 @@ interface LayoutProps {
   header: ReactNode;
   content: ReactNode;
   footer: ReactNode;
-  sidebar: ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({
-  header,
-  content,
-  footer,
-  sidebar,
-}) => {
+const Layout: React.FC<LayoutProps> = ({ header, content, footer }) => {
   return (
     <div className={styles.layoutContainer}>
       <header className={styles.header}>{header}</header>
       <div className="d-flex">
-        <aside className={`${styles.sidebar} position-fixed`}>{sidebar}</aside>
         <main className={styles.content}>{content}</main>
       </div>
       <footer className={styles.footer}>{footer}</footer>
