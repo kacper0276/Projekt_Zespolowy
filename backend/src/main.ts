41b5427f555c438472adc1a0e885ddb4d1eec227
kacper0276 <kacper0276@op.pl>
2025-03-04 10:35:18 +0100

    [BE] Add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Projekt zespołowy - Swagger')
+    .setDescription('Swagger przygotowany dla testowania API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory(), {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
