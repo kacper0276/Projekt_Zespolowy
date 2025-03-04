da5bbb3b50243bc3009107a8477395311786503e
kacper0276 <kacper0276@op.pl>
2025-03-04 13:59:12 +0100

    [BE] Remove imports

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello() {
-    return `${process.env.DB_HOST} ${process.env.DB_PORT} ${process.env.DB_USER}`;
-  }
 }
