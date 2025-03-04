a3f970d55c9928889065545b520d6af0a04cc65e
Tomasz2002 <121009782+Tomasz2002@users.noreply.github.com>
2025-03-04 23:49:01 +0100

    [Frontend] Fixing styling on MainPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Header from "../src/layout/Header/Header"
 import Footer from "../src/layout/Footer/Footer"
 import MainPage from "../src/pages/MainPage/MainPage"
