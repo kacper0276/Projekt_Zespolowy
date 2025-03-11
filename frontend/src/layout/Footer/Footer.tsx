44863fd88f37b1c51edc34429fcb1a2aff1c6f4e
kacper0276 <kacper0276@op.pl>
2025-03-11 14:13:18 +0100

    [FE] Small fix

--- frontend/src/layout/Footer/Footer.tsx
+++ frontend/src/layout/Footer/Footer.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './Footer.module.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import styles from "./Footer.module.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Footer: React.FC = () => {
   return (
@@ -16,21 +16,27 @@ const Footer: React.FC = () => {
           <div className="col-md-4">
             <h5 className={styles.linkTitle}>Szybkie linki</h5>
             <ul className={styles.footerLinks}>
-              <li><a href="/features">Funkcje</a></li>
-              <li><a href="/pricing">Cennik</a></li>
+              <li>
+                <a href="/features">Funkcje</a>
+              </li>
+              <li>
+                <a href="/pricing">Cennik</a>
+              </li>
             </ul>
           </div>
           <div className="col-md-4">
             <h5 className={styles.linkTitle}>Kontakt</h5>
             <div className={styles.socialLinks}>
-              <a href="#" className={styles.socialIcon}>GitHub</a>
+              <a href="#" className={styles.socialIcon}>
+                GitHub
+              </a>
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col-12 text-center">
             <p className={styles.copyright}>
-              © 2024 KanbanPro. Wszelkie prawa zastrzeżone.
+              © 2025 KanbanPro. Wszelkie prawa zastrzeżone.
             </p>
           </div>
         </div>
