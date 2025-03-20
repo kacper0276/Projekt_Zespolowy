b846e8ec3de46615942fae0c4b08db9af8be8b56
kacper0276 <kacper0276@op.pl>
2025-03-20 18:17:17 +0100

    [BE] Create mailer config

---
+++ backend/src/config/mailer.config.ts
@@ -0,0 +1,27 @@
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const mailerConfig: any = {
+  transport: {
+    host: 'smtp.poczta.onet.pl',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_LOGIN,
+      pass: process.env.EMAIL_PASSWORD,
+      method: 'LOGIN',
+    },
+  },
+  defaults: {
+    from: '"Adminisjtracja serwisu" <kacper4312@op.pl>',
+  },
+  template: {
+    dir: process.cwd() + '/template/',
+    adapter: new HandlebarsAdapter(),
+    options: {
+      strict: true,
+    },
+  },
+};
