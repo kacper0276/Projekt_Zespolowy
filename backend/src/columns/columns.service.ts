b1660b175468a71eb3d5edcbcb457baa9120a844
kacper0276 <kacper0276@op.pl>
2025-03-21 21:35:59 +0100

    [BE] Small fix in columns

--- backend/src/columns/columns.service.ts
+++ backend/src/columns/columns.service.ts
@@ -21,14 +21,20 @@ export class ColumnsService {
   ): Promise<ColumnEntity> {
     const kanban = await this.kanbanRepository.findOne({
       where: { id: +kanbanId },
+      relations: ['columns'],
     });
     if (!kanban) {
       throw new Error('Kanban not found');
     }
 
+    const maxOrder = kanban.columns.reduce((max, column) => {
+      return column.order > max ? column.order : max;
+    }, -1);
+
     const column = this.columnRepository.create({
       ...createColumnDto,
       kanban,
+      order: maxOrder + 1,
     });
 
     return await this.columnRepository.save(column);
