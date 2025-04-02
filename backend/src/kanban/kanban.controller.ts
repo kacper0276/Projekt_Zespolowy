bfbc2603ad6160ae5d0a8c2526febfebc5c6dcde
kacper0276 <kacper0276@op.pl>
2025-04-02 08:38:32 +0200

    [BE] Fix in kanban controller

--- backend/src/kanban/kanban.controller.ts
+++ backend/src/kanban/kanban.controller.ts
@@ -52,6 +52,8 @@ export class KanbanController {
     @Res() response: Response,
   ) {
     try {
+      console.log(createKanbanDto);
+
       let base64Image = null;
       if (file) {
         const filePath = `../frontend/public/kanbanBackgroundImg/${file.filename}`;
@@ -60,7 +62,12 @@ export class KanbanController {
         createKanbanDto.backgroundImage = base64Image;
       }
 
+      if (!createKanbanDto.backgroundImage)
+        createKanbanDto.backgroundImage = '';
+
       const res = await this.kanbanService.createKanban(createKanbanDto);
+
+      console.log(res);
 
       response.status(HttpStatus.CREATED).send({
         message: 'kanban-board-created',
