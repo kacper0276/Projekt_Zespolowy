2b73d68b3cf7a3893598f78fd84aed0328265c82
kacper0276 <kacper0276@op.pl>
2025-04-08 14:22:52 +0200

    [FE] Init Team page

---
+++ frontend/src/pages/TeamPage/TeamPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./TeamPage.module.scss";
+
+const TeamPage: React.FC = () => {
+  return (
+    <div className={styles.teamPage}>
+      <h1>Team page</h1>
+    </div>
+  );
+};
+
+export default TeamPage;
