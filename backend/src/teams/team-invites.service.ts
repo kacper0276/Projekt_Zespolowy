a4e0b6a01ae3a3defbdbb6c8c0b11d40b27db075
kacper0276 <kacper0276@op.pl>
2025-05-07 09:11:38 +0200

    [BE] Team invites serivce changes

--- backend/src/teams/team-invites.service.ts
+++ backend/src/teams/team-invites.service.ts
@@ -4,12 +4,16 @@ import { Repository } from 'typeorm';
 import { TeamInvite } from './entities/team-invite.entity';
 import { UpdateTeamInviteDto } from './dto/update-team-invite.dto';
 import { CreateTeamInviteDto } from './dto/create-team-invite.dto';
+import { User } from '../users/entities/user.entity';
+import { first } from 'rxjs';
 
 @Injectable()
 export class TeamInitesService {
   constructor(
     @InjectRepository(TeamInvite)
     private readonly teamInviteRepository: Repository<TeamInvite>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async createInvite(
@@ -34,11 +38,46 @@ export class TeamInitesService {
     return invite;
   }
 
-  async getInvitesByUserId(userId: string): Promise<TeamInvite[]> {
-    return this.teamInviteRepository.find({
+  async getInvitesByUserId(
+    userId: string,
+  ): Promise<
+    (TeamInvite & {
+      invitedByUser: {
+        email: string;
+        firstName: string | null;
+        lastName: string | null;
+      } | null;
+    })[]
+  > {
+    const invites = await this.teamInviteRepository.find({
       where: { user: { id: +userId } },
       relations: ['team'],
     });
+
+    const invitesWithInviter = await Promise.all(
+      invites.map(async (invite) => {
+        const inviter = await this.userRepository.findOne({
+          where: { id: invite.invitedBy },
+        });
+
+        return {
+          id: invite.id,
+          team: invite.team,
+          invitedBy: inviter.id,
+          invitedByUser: inviter
+            ? {
+                email: inviter.email,
+                firstName: inviter.firstName,
+                lastName: inviter.lastName,
+              }
+            : null,
+          status: invite.status,
+          user: invite.user,
+        };
+      }),
+    );
+
+    return invitesWithInviter;
   }
 
   async updateInvite(
--- backend/src/teams/teams.module.ts
+++ backend/src/teams/teams.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from './entities/team.entity';
 import { TeamInvite } from './entities/team-invite.entity';
 import { TeamInitesService } from './team-invites.service';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team, TeamInvite])],
+  imports: [TypeOrmModule.forFeature([Team, TeamInvite, User])],
   controllers: [TeamsController],
   providers: [TeamsService, TeamInitesService],
 })
